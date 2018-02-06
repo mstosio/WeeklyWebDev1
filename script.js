b888a4e0b2117ca1b7c85b9bf0fbf70572dcc623
Michal Stosio <michaustosio@gmail.com>
2018-02-06 15:11:45 +0100

    added project

---
+++ script.js
@@ -0,0 +1,19 @@
+var button = document.getElementsByClassName("radio");
+
+// poprawka
+
+button[0].addEventListener("click", function(){
+	button[0].classList.toggle("radioStyle");
+
+});
+
+
+button[1].addEventListener("click", function(){
+	button[1].classList.toggle("radioStyle");
+
+});
+
+button[2].addEventListener("click", function(){
+	button[2].classList.toggle("radioStyle");
+
+});
